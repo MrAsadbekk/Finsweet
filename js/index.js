ada2933846d4b524aef7035e75b23f318904c733
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-10 01:24:31 +0500

    add search

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ menuBtn.addEventListener("click", () => {
 
 const cardBox = document.querySelector(".card__box");
 
-const myKey = "e6721b7844fa4e1db86df00f773e0381";
-const url = "https://newsapi.org/v2/everything?q=Apple&apiKey=";
+let url =
+  "https://newsapi.org/v2/everything?q=Apple&apiKey=e6721b7844fa4e1db86df00f773e0381";
 
-// fetch(`${url}${myKey}`)
+// fetch(`${url}`)
 //   .then((response) => response.json())
 //   .then((data) => {
 //     console.log(data.articles);
@@ -52,9 +52,7 @@ const url = "https://newsapi.org/v2/everything?q=Apple&apiKey=";
 //   })
 //   .catch((error) => console.log(error));
 
-const searchBox = document.querySelector(".search__box");
-
-fetch(`${url}${myKey}`)
+fetch(`${url}`)
   .then((response) => response.json())
   .then((data) => {
     const newData = data.articles.slice(0, 4);
@@ -92,3 +90,73 @@ fetch(`${url}${myKey}`)
     });
   })
   .catch((error) => console.log(error));
+
+const searchBox = document.querySelector(".search__box");
+const searchForm = document.querySelector(".search__form");
+let searchQuary = "Apple";
+const searchUrl = `https://newsapi.org/v2/everything?q=${searchQuary}&apiKey=e6721b7844fa4e1db86df00f773e0381`;
+const search = document.querySelector(".search__input");
+
+async function fetchData() {
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    searchQuary = searchTerm;
+    url = `https://newsapi.org/v2/everything?q=${searchQuary}&apiKey=e6721b7844fa4e1db86df00f773e0381`;
+  } else {
+    url =
+      "https://newsapi.org/v2/everything?q=Apple&apiKey=e6721b7844fa4e1db86df00f773e0381";
+  }
+
+  console.log(url);
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    searchBox.innerHTML = "";
+
+    if (data.articles.length === 0) {
+      const notFound = document.createElement("p");
+      notFound.classList.add("search__notFound");
+      notFound.textContent = "!Nothing found";
+
+      searchBox.appendChild(notFound);
+    } else {
+      const newData = data.articles.slice(6, 12);
+
+      newData.map((data) => {
+        const minibox = document.createElement("div");
+        minibox.classList.add("search__minibox");
+
+        const searchImg = document.createElement("img");
+        searchImg.classList.add("search__img");
+        searchImg.src = data.urlToImage;
+
+        const textBox = document.createElement("div");
+
+        const searchTitle = document.createElement("h3");
+        searchTitle.classList.add("search__title");
+        searchTitle.textContent = data.title;
+
+        const searchTextTwo = document.createElement("p");
+        searchTextTwo.classList.add("search__text-two");
+        searchTextTwo.textContent = data.content;
+
+        textBox.appendChild(searchTitle);
+        textBox.appendChild(searchTextTwo);
+        minibox.appendChild(searchImg);
+        minibox.appendChild(textBox);
+        searchBox.appendChild(minibox);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetchData();
+});
