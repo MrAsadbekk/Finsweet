097913923d35d3a05811817049349a2a84ade18e
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-14 17:05:39 +0500

    register fixed remove

--- js/register.js
+++ js/register.js
@@ -19,43 +19,70 @@ const formSpan = document.querySelectorAll(".register__span");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (!fname.value) {
-    formSpan[0].textContent = "!Enter information";
-  } else if (fname.value.length < 5) {
-    formSpan[0].textContent = "!Firstname must be more than 5 characters long";
-  } else {
-    formSpan[0].textContent = "";
+  function fnameValidation() {
+    if (!fname.value) {
+      return (formSpan[0].textContent = "!Enter information");
+    } else if (fname.value.length < 5) {
+      return (formSpan[0].textContent =
+        "!Firstname must be more than 5 characters long");
+    } else {
+      return (formSpan[0].textContent = "");
+    }
   }
 
-  if (!lname.value) {
-    formSpan[1].textContent = "!Enter information";
-  } else if (lname.value.length < 5) {
-    formSpan[1].textContent = "!Lastname must be more than 5 characters long";
-  } else {
-    formSpan[1].textContent = "";
+  function lnameValidation() {
+    if (!lname.value) {
+      return (formSpan[1].textContent = "!Enter information");
+    } else if (lname.value.length < 5) {
+      return (formSpan[1].textContent =
+        "!Lastname must be more than 5 characters long");
+    } else {
+      return (formSpan[1].textContent = "");
+    }
   }
 
-  if (!uname.value) {
-    formSpan[2].textContent = "!Enter information";
-  } else {
-    formSpan[2].textContent = "";
+  function unameValidation() {
+    if (!uname.value) {
+      return (formSpan[2].textContent = "!Enter information");
+    } else {
+      return (formSpan[2].textContent = "");
+    }
   }
 
-  if (!password.value) {
-    formSpan[3].textContent = "!Enter information";
-  } else if (password.value.length < 8) {
-    formSpan[3].textContent = "!minimum password length should be 8 characters";
-  } else {
-    formSpan[3].textContent = "";
+  function passwordValidation() {
+    if (!password.value) {
+      formSpan[3].textContent = "!Enter information";
+    } else if (password.value.length < 8) {
+      formSpan[3].textContent =
+        "!minimum password length should be 8 characters";
+    } else {
+      formSpan[3].textContent = "";
+    }
   }
 
-  if (!cpassword.value) {
-    formSpan[4].textContent = "!Enter information";
-  } else if (password.value !== cpassword.value) {
-    formSpan[4].textContent = "!Password not confirmed";
-  } else {
-    formSpan[4].textContent = "";
+  function cpasswordValidation() {
+    if (!cpassword.value) {
+      formSpan[4].textContent = "!Enter information";
+    } else if (password.value !== cpassword.value) {
+      formSpan[4].textContent = "!Password not confirmed";
+    } else {
+      formSpan[4].textContent = "";
+    }
   }
 
-  console.log(formSpan[0]);
+  fnameValidation();
+  lnameValidation();
+  unameValidation();
+  passwordValidation();
+  cpasswordValidation();
+
+  if (
+    !fnameValidation() &&
+    !lnameValidation() &&
+    !unameValidation() &&
+    !passwordValidation() &&
+    !cpasswordValidation()
+  ) {
+    return console.log(true);
+  }
 });
